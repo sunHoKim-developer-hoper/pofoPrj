f15d59af13d07714135d23ef73952350553699cb
KIM <gimseonho4@gmail.com>
2025-04-08 17:25:35 +0900

    application.properties의 보안성 강화

--- backend/src/main/java/kr/co/pofo/pofoapiboot3/PofoApiBoot3Application.java
+++ backend/src/main/java/kr/co/pofo/pofoapiboot3/PofoApiBoot3Application.java
@@ -2,11 +2,18 @@ package kr.co.pofo.pofoapiboot3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.PropertySource;
+
+import kr.co.pofo.pofoapiboot3.config.DotenvLoader;
 
 @SpringBootApplication
+//@PropertySource(value = {"classpath:${spring.profiles.active:dev}.properties"}) 활성화된 profile 설정 가능 -> spring.profiles.active를 활용하는 방식으로 결정
 public class PofoApiBoot3Application {
 
   public static void main(String[] args) {
+    
+    //설정값 세팅 로직
+    DotenvLoader.loadEnv();
     SpringApplication.run(PofoApiBoot3Application.class, args);
   }
 }
